473f00b0d87f95ff2855f4347598c407564201eb
ferranodriozola <ferranodriozola@gmail.com>
2026-05-09 23:41:14 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -166,7 +166,7 @@ with st.spinner('Processant dades...'):
     try:
         df = llegir_full(URL_XLSX, full_seleccionat)
 
-        df_filtrat = df.iloc[0:1000, 0:11]
+        df_filtrat = df.iloc[0:1000, 0:12]
 
         if full_seleccionat == 'listPerson':
             st.subheader("Personatges en format XML")
